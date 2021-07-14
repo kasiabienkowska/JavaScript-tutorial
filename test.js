15c8575150df999a86111d2fbddfb4cd1dbe39c8
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-07-14 21:38:53 +0200

    else if statement

--- test.js
+++ test.js
@@ -1,13 +1,11 @@
-var youLikeMeat = true;
-var myNum = 10;
+var myAge = 29;
 
-if (youLikeMeat){
-
-    document.write("you like meat");
-
-}
-
-else{
-
-    document.write("you hate meat");
+if (myAge > 30){
+    document.write("You are over 30");
+} else if (myAge >20) {
+    document.write("You are over 20");
+} else if(myAge > 10){
+    document.write("You are over 10");
+} else {
+    document.write("You are not over 10");
 }
