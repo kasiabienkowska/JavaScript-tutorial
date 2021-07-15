65680a9808245dfbda6b67c3da789761cd3b5658
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-07-15 16:58:33 +0200

    STrings

--- test.js
+++ test.js
@@ -1,16 +1,29 @@
-var a = "hello";
-var b = 5;
+//var myString = 'I\'m a "fun ninja" string';
 
-//double negative
-if (!isNaN(a)){
-    
-    console.log("meaning of life is "+ (a *b));
+/*console.log(myString.length); // zwraca ilosc liter*/
+
+/*console.log(myString.toUpperCase()); // zwraca duze litery*/
+
+/*console.log(myString.toLowerCase()); //zwraca małe litery */
+
+/*console.log(myString.indexOf("string")); // zwraca w którym momencie zaczyna się dane słowo */
+
+/*if (myString.indexOf("ninja") === -1){
+    console.log("the word ninja is not in the string");
 }
-
 else{
-    console.log("that aint even a number, thickie");
-    
+    console.log("the word ninja starts at position " + myString.indexOf("ninja"));
 }
+*/
 
+/*var String1 = "abc";
 
+var String2 = "ABC";
 
+console.log(String1.toLowerCase() === String2.toLowerCase());*/
+
+var String1 = "abc";
+
+var String2 = "bcd";
+
+console.log(String1 < String2); /* jest to prawda bo "a" jest przed "b" w alfabecie, natomiast jeżeli B byłoby z wielkiej litery to będzie false */
