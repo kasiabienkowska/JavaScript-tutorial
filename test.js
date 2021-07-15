7f1fec80d3859e863456e0d96c08c8af8665db49
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-07-15 22:43:05 +0200

    contructor functions

--- test.js
+++ test.js
@@ -1,15 +1,27 @@
-var myCar2 = {
+ 
+    var Car =function(maxSpeed, driver) {
 
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function(speed, time){
-        console.log(speed * time);
-    },
-    logDriver: function(){
-        console.log("driver name is " + this.driver);
+        this.maxSpeed = maxSpeed;
+        this.driver = driver;
+        this.drive = function (speed, time) {
+            console.log(speed * time);
+
+        };
+
+        this.logDriver = function()
+        {
+            console.log("driver name is " + this.driver);
+
+        };
+
     }
-};
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+var myCar = new Car(70, "Ninja Man");
+var myCar2 = new Car(40, "Kasia");
+var myCar3 = new Car(10, "Srasia");
+var myCar4 = new Car(90, "James Bond");
+
+myCar.drive(30,5);
+myCar3.logDriver();
+
+
