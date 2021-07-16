8fc4017ed51330f62b752b391d1cb752048cf9e9
Kasia Bienkowska <kasia.bienkowska03@gmail.com>
2021-07-16 21:08:16 +0200

    The onClick Event

--- test.js
+++ test.js
@@ -1,35 +1,16 @@
-/*var myDate = new Date();
-console.log(myDate);
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
-var myPastDate = new Date(1545, 11,2, 10, 30,15) ;
-var myFutureDate = new Date(2515, 0,31, 10, 30,15);
+button.onclick = function(){
 
-console.log(myPastDate);
-console.log(myFutureDate); */
+    if(content.className == "open"){
+        //shrink the box
+        content.className = "";
+        button.innerHTML = "Show More";
+    } else{
+        //expand the box 
+        content.className = "open";
+        button.innerHTML = "Show Less";
+    }
 
-var birthday = new Date(1985, 0, 15, 11, 15, 25);
-var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
-
-//get the month of the date (0-11)
-console.log(birthday.getMonth());
-
-//get the full year (YYYY)
-console.log(birthday.getFullYear());
-
-//get the date of the month
-console.log(birthday.getDate());
-
-//get the day of the week (0-6)
-console.log(birthday.getDay());
-
-//get the hour od the date (0-23)
-console.log(birthday.getHours());
-
-//get the number of miliseconds since 1st Jan 1970
-console.log(birthday.getTime());
-
-if(birthday.getTime()==birthday2.getTime()){
-    console.log("birthdays are equal");
-} else{
-    console.log("birthdays are not equal");
-}
+};
